47fe8427f59fdc7ba72de4d5c211930910a3f577
tomato111 <eidaitanaka10@gmail.com>
2021-12-24 01:26:59 +0900

    できちゃ！

--- src/main/java/com/github/ozmt/crafting_miss/craftingmiss/CraftingMiss.java
+++ src/main/java/com/github/ozmt/crafting_miss/craftingmiss/CraftingMiss.java
@@ -1,17 +1,34 @@
 package com.github.ozmt.crafting_miss.craftingmiss;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.CraftItemEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class CraftingMiss extends JavaPlugin {
+import java.util.Random;
+
+public final class CraftingMiss extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        Bukkit.getServer().getPluginManager().registerEvents(this, this);
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    @EventHandler
+    public void craftingMiss(CraftItemEvent event) {
+        double random = Math.random();
+        if (random < 0.5D) {
+            event.setCurrentItem(new ItemStack(Material.COAL));
+        }
+
     }
 }
